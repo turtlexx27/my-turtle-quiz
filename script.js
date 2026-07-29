a74719f09f086570a6bed43e116e33622b4f96b0
turtlexx27 <a0908783159@gmail.com>
2026-07-29 18:54:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -464,8 +464,8 @@ function animateTurtles() {
     
     if (winWidth <= 900) {
         // 這是手機版的設定
-        shiftX = -300; 
-        shiftY = -300; 
+        shiftX = -100; 
+        shiftY = -100; 
     } else {
         // 這是電腦版的設定
         shiftX = 40; 
